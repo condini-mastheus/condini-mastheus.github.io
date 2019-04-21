34f4bd2a1a295b85a287dfe0820c2bb8a8123d4d
condini-mastheus <matheus.condini@gmail.com>
2019-04-21 11:29:48 -0300

    Updates

--- precache-manifest.83553a433acf3cb007fea1aac3d0391f.js
+++ precache-manifest.83553a433acf3cb007fea1aac3d0391f.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "31bbf52984091510ed5e",
-    "url": "/static/js/main.b731aef7.chunk.js"
+    "revision": "4b60d88caecd6733a84e",
+    "url": "/static/js/main.b539f856.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -12,7 +12,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.dc85911c.chunk.js"
   },
   {
-    "revision": "99a208ab1e583db040a1e4cf8ab4b725",
+    "revision": "4cf47c59fb2968d1fa7dc309046261ed",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b30cdce63d3fa5adaa990f5d5c5cdd09.js"
+  "/precache-manifest.83553a433acf3cb007fea1aac3d0391f.js"
 );
 
 workbox.clientsClaim();
